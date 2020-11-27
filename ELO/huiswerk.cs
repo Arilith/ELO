5232402f6261d1fcb14ad28f1d1b6c69ee9b7f7a
Tristan van Triest <tristantriest@gmail.com>
2020-11-27 10:50:13 +0100

    Remake Huiswerk

--- ELO/huiswerk.cs
+++ ELO/huiswerk.cs
@@ -1,27 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ELO
-{
-    class huiswerk
-    {
-        private int _cijfers;
-
-        public string SetDueDate()
-        {
-
-            return _cijfers;
-        }
-
-        public string SetGroup()
-        {
-
-        }
-
-        public void SetCijfers(int cijfers)
-        {
-            _cijfers = cijfers;
-        }
-    }
-}
+﻿// using System;
+// using System.Collections.Generic;
+// using System.Text;
+//
+// namespace ELO
+// {
+//     class huiswerk
+//     {
+//         private int _cijfers;
+//
+//         public string SetDueDate()
+//         {
+//
+//             return _cijfers;
+//         }
+//
+//         public string SetGroup()
+//         {
+//
+//         }
+//
+//         public void SetCijfers(int cijfers)
+//         {
+//             _cijfers = cijfers;
+//         }
+//     }
+// }
