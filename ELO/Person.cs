281e65ec8965edb5c4b62d4df993299bab198c6a
Tristan van Triest <tristantriest@gmail.com>
2020-11-27 10:23:07 +0100

    Changed Person

---
+++ ELO/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ELO
+{
+    public class Person
+    {
+
+        public string Name { get; private set; }
+
+
+
+        public Person(string _name, int _age)
+        {
+            this.Name = _name;
+            this.Age = _age;
+        }
+
+        public Person(string _name)
+        {
+            this.Name = _name;
+        }
+
+        public override string ToString()
+        {
+            return $"Mijn naam is: {name} en ik ben {age} jaar oud.";
+        }
+    }
+}
