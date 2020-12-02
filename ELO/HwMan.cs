2631f3293d3d052bf27521791d1f7d2a15a795b2
Teun Strijbosch <436470@student.fontys.nl>
2020-12-02 10:09:44 +0100

    Homework menagement

---
+++ ELO/HwMan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class HwMan
+{
+	public HwMan()
+	{
+		 public List<huiswerk> huiswerkList;
+
+        public HwMan()
+        {
+            huiswerkList = new List<huiswerk>();
+        }
+
+    public void AddHomeworkToList(string work, string subject, string dueDate, string _class)
+    {
+        huiswerkList.Add(new Homework(subject, work, dueDate, "Huiswerk", _class));
+    }
+
+}
+}
