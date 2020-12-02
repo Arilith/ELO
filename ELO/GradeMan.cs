7f4703a2454327e1ecbe0b694ea3eadf449759d8
Uhud kemal Yurttagül <460068@student.fontys.nl>
2020-12-02 15:16:33 +0100

    grade grademan

---
+++ ELO/GradeMan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ELO
+{
+    class GradeMan
+    {
+        public static List<Grade> gradelist {get; private set;}
+        public GradeMan()
+        {
+            gradelist = new List<Grade>();
+        }
+    }
+}
