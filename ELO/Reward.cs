9aa396f4adf9e51a120732f404226284b5ac5ddc
Teun Strijbosch <436470@student.fontys.nl>
2021-01-04 11:03:15 +0100

    Rewards

---
+++ ELO/Reward.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ELO
+{
+	public class Reward
+	{
+		public string ImageURL { get; private set; }
+		public string Rewards { get; private set;  }
+	}
+}
