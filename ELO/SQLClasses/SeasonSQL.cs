1f12f48af7500b23cc67a9e9166a87a7ad847dca
Uhud kemal Yurttagül <460068@student.fontys.nl>
2021-01-04 15:26:11 +0100

    database

---
+++ ELO/SQLClasses/SeasonSQL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace ELO.SQLClasses
+{
+    public class SeasonSQL
+    {
+        private string UUID = "lul";
+        private MySqlManager mySqlManager;
+        public SeasonSQL()
+        {
+            mySqlManager = new MySqlManager();
+        }
+
+        public void AddSeasonToDatabase(string startDate, string endDate, string seasonName)
+        {
+            MySqlCommand addSeasonCommand = new MySqlCommand($"insert into seasons (name, endDate, startDate, UUID) values (@name, @endDate, @startDate, @UUID)", mySqlManager.con);
+
+            addSeasonCommand.Parameters.AddWithValue("@name", seasonName);
+            addSeasonCommand.Parameters.AddWithValue("@endDate", endDate);
+            addSeasonCommand.Parameters.AddWithValue("@startDate", startDate);
+            addSeasonCommand.Parameters.AddWithValue("@UUID", UUID);
+            addSeasonCommand.Prepare();
+
+            addSeasonCommand.ExecuteNonQuery();
+
+        }
+    }
+}
---
+++ Front-End/AddSeason.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ELO.Managers;
+
+namespace Front_End
+{
+    public partial class AddSeason : System.Web.UI.Page
+    {
+        SeasonMan seasonMan;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            seasonMan = new SeasonMan();
+            if (IsPostBack)
+            {
+                string startDate = Request.Form["startdate"];
+                string endDate = Request.Form["enddate"];
+                string seasonName = Request.Form["seasonname"];
+
+                seasonMan.AddSeason(startDate, endDate, seasonName);
+            }
+
+        }
+    }
+}
