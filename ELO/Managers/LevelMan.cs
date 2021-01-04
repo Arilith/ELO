0461e22c3921ae45b717e9e5e89fcee77be0dbfa
Arilith <tristantriest@gmail.com>
2021-01-04 15:23:37 +0100

    LevelMan uitleg

--- ELO/Managers/LevelMan.cs
+++ ELO/Managers/LevelMan.cs
@@ -8,10 +8,18 @@ namespace ELO.Managers
     public class LevelMan
     {
 
+        private LevelSQL levelSql;
+
         public LevelMan()
+        {
+            levelSql = new LevelSQL();
+        }
+
+        public void AddLevel(int levelNummer, int requiredExp, string seizoen)
         {
 
         }
+
 
     }
 }
