785c5d96d817df744b883894bae9caea562e0f79
Teun Strijbosch <436470@student.fontys.nl>
2021-01-05 10:24:01 +0100

    Rewards SQL

--- ELO/Reward.cs
+++ ELO/Reward.cs
@@ -6,14 +6,14 @@ namespace ELO
 {
 	public class Reward
 	{
-		public string Rewards { get; private set;  }
+		public string Title { get; private set;  }
 		public string RewardDescription { get; private set; }
 		public string ImageURL { get; private set; }
-		public Level RequiredLevel { get; private set; }
+		public int RequiredLevel { get; private set; }
 
-		public Reward(string rewards, string rewardDescription, string imageURL, Level requiredLevel)
+		public Reward(string title, string rewardDescription, string imageURL, int requiredLevel)
 		{
-			this.Rewards = rewards;
+			this.Title = title;
 			this.RewardDescription = rewardDescription;
 			this.ImageURL = imageURL;
 			this.RequiredLevel = requiredLevel;
--- Front-End/AddRewards.aspx.cs
+++ Front-End/AddRewards.aspx.cs
@@ -4,14 +4,26 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ELO.Managers;
 
 namespace Front_End
 {
 	public partial class AddRewards : System.Web.UI.Page
 	{
+		RewardMan rewardMan;
+
 		protected void Page_Load(object sender, EventArgs e)
 		{
+			rewardMan = new RewardMan();
+			if (IsPostBack)
+			{
+				string title = Request.Form["title"];
+				string rewardDescription = Request.Form["rewardDescription"];
+				string imageURL = Request.Form["imageURL"];
+				int requiredLevel = Convert.ToInt32(Request.Form["requiredLevel"]);
 
+				rewardMan.AddReward(title, rewardDescription, imageURL, requiredLevel);
+			}
 		}
 	}
 }
