ed876783a025f71a2a83e6abb9c2a4fd8dbbb3f1
Teun Strijbosch <436470@student.fontys.nl>
2021-01-12 14:00:23 +0100

    Radiobuttons ipv checkboxes in check attendance

--- Front-End/CheckAttendance.aspx.cs
+++ Front-End/CheckAttendance.aspx.cs
@@ -8,20 +8,53 @@ using ELO;
 
 namespace Front_End
 {
-	public partial class CheckAttendance : System.Web.UI.Page
-	{
-		public ClassManager classManager;
-		public UserMan userManager;
-		public Person loggedInPerson;
-		protected void Page_Load(object sender, EventArgs e)
-		{
-			classManager = new ClassManager();
-			userManager = new UserMan();
+    public partial class CheckAttendance : System.Web.UI.Page
+    {
+        public ClassManager classManager;
+        public HwMan hwMan;
+        public SubjectManager subjectManager;
+        public Person LoggedInPerson;
 
-			loggedInPerson = (Person)Session["person"];
-			
-		}
 
-		
-	}
+        public int RB = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LoggedInPerson = (Person)Session["person"];
+            classManager = new ClassManager();
+            hwMan = new HwMan();
+            subjectManager = new SubjectManager();
+
+            if (IsPostBack)
+            {
+                ConvertAndInsertData();
+            }
+        }
+        
+        private void ConvertAndInsertData()
+        {
+            string presentString = Request.Form["present"];
+            string absentString = Request.Form["absent"];
+            string leaveString = Request.Form["leave"];
+            string lateString = Request.Form["late"];
+            string lateAllowString = Request.Form["lateAllow"];
+            string forgotString = Request.Form["forgot"];
+            string expelledString = Request.Form["expelled"];
+            
+            bool absent = absentString == "on";
+            bool present = presentString == "on";
+            bool leave = leaveString == "on";
+            bool late = lateString == "on";
+            bool lateAllow = lateAllowString == "on";
+            bool forgot = forgotString == "on";
+            bool expelled = expelledString == "on";
+
+            if (absent == true)
+                    {
+                Request.Form["present"] = "off";
+                    }
+
+        OutputLabel.Text = "Huiswerk ingevoerd!";
+        }
+
+    }
 }
