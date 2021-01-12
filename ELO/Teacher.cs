8932b417787072d33c105cfe61f32e5584807fb7
Teun Strijbosch <436470@student.fontys.nl>
2021-01-12 14:30:58 +0100

    exp voor persoon toegevoegd

--- ELO/Teacher.cs
+++ ELO/Teacher.cs
@@ -15,19 +15,19 @@ namespace ELO
 
         public Class MentorForClass { get; private set; } 
         
-        public Teacher(string name, int age, string school, string type, Subject subject, Class _mentorClass, string userId, string registrationDate, string userName, string email) : base(name, age, school, type, userName, userId, registrationDate, email)
+        public Teacher(string name, int age, string school, string type, Subject subject, Class _mentorClass, string userId, string registrationDate, string userName, string email, int exp) : base(name, age, school, type, userName, userId, registrationDate, email, exp)
         {
             this.Subject = subject;
 
             this.MentorForClass = _mentorClass;
         }
 
-        public Teacher(string name, int age, string school, string type, Subject subject, string userId, string registrationDate, string userName, string email) : base(name, age, school, type, userName, userId, registrationDate, email)
+        public Teacher(string name, int age, string school, string type, Subject subject, string userId, string registrationDate, string userName, string email, int exp) : base(name, age, school, type, userName, userId, registrationDate, email, exp)
         {
             this.Subject = subject;
         }
 
-        public Teacher(string name, int age, string school, string type, string userId, string registrationDate, string userName, string email) : base(name, age, school, type, userName, userId, registrationDate, email)
+        public Teacher(string name, int age, string school, string type, string userId, string registrationDate, string userName, string email, int exp) : base(name, age, school, type, userName, userId, registrationDate, email, exp)
         {
             
         }
